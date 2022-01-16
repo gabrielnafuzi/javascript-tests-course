8720c1a909c7aeb16a281af7fd5a8a05c158a4c2
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-01-16 00:49:10 -0300

    add tests to sum method

--- module-1/project-1/src/lib/calculator.js
+++ module-1/project-1/src/lib/calculator.js
@@ -1,4 +1,13 @@
-const sum = (a, b) => a + b
+const sum = (a, b) => {
+  const parsedA = parseInt(a, 10)
+  const parsedB = parseInt(b, 10)
+
+  if (isNaN(parsedA) || isNaN(parsedB)) {
+    throw new Error('Cannot sum non-numbers')
+  }
+
+  return parsedA + parsedB
+}
 
 module.exports = {
   sum
